dc48fcee60fd0b14179336dbdd1b955848364aec
Andrea Manzi <manzolo@libero.it>
2026-04-26 10:03:45 +0200

    feat: add nano, vim, zsh with agnoster theme

--- run.sh
+++ run.sh
@@ -94,6 +94,10 @@ packages:
   - curl
   - gnupg2
   - lsb-release
+  - zsh
+  - vim
+  - nano
+  - fonts-powerline
 write_files:
   - path: /etc/profile.d/cloud-init-status.sh
     permissions: '0755'
@@ -202,6 +206,13 @@ write_files:
       conn.commit()
       conn.close()
       "
+  - path: /tmp/setup-zsh.sh
+    permissions: '0755'
+    content: |
+      #!/bin/bash
+      RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+      sed -i 's/^ZSH_THEME=.*/ZSH_THEME="agnoster"/' ~/.zshrc
+      sed -i 's/^plugins=(.*)/plugins=(git)/' ~/.zshrc
 runcmd:
   - chmod -x /etc/update-motd.d/*
   - sed -i 's/^#\?PrintMotd.*/PrintMotd yes/' /etc/ssh/sshd_config
@@ -230,6 +241,8 @@ runcmd:
     # Install and configure pgAdmin4
     bash /tmp/pgadmin_setup.sh
   - chown -R labuser:labuser /home/labuser
+  - sudo -Hu labuser bash /tmp/setup-zsh.sh
+  - chsh -s /usr/bin/zsh labuser
   - echo "=== postgres-lab VM is ready! ==="
 USERDATA
 
